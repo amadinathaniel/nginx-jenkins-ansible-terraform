efe7a6636faf14dc04fe6f45a0c170c330140b31
Nathaniel Amadi <amadinathaniel@gmail.com>
2022-07-15 04:19:36 +0100

    Updated variables.tf with default values

--- terraform/static-site/variables.tf
+++ terraform/static-site/variables.tf
@@ -1,15 +1,19 @@
 variable "profile" {
+    default = "DevOpsUser"
     description = "The profile used to auth to AWS"
 }
 
 variable "region" {
+    default = "eu-west-2"
     description= "The region our instance will be in (i.e. us-east-1)"
 }
 
 variable "name" {
+    default = "hello"
     description= "The name of the instance we are creating"
 }
 
 variable "group" {
+    default = "web"
     description= "the name of the group we will be using for Ansible purposes"
 }
